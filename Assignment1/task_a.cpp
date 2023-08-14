c2cc334dec3e60b8ca36d771ac3c94bdd745dd14
Daniel Bjørdal <danbjo2001@gmail.com>
2023-08-14 19:08:24 +0200

    task a finished(one weird formatting issue when recieving the last input entry but everything works fine)

--- Assignment1/task_a.cpp
+++ Assignment1/task_a.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 /**
- * Amount of samples to be taken
+ * Antall temperaturer som skal sendes inn
  */
-const int length = 5;
+const int temperatures = 5;
 
 
 /**
- * Amount of samples below 10, between 10 and 20(including 10 and 20), and above 20.
+ * Antall temperaturer innsendt under 10, mellom 10 og 20(inkludert 10 og 20), og over 20
  */
 int below = 0;
 
@@ -19,19 +19,27 @@ int above = 0;
 
 int main() {
     double temp;
-    cout << "Du skal skrive inn " << length << " temperaturer.";
+    cout << "Du skal skrive inn " << temperatures << " temperaturer." <<endl;
 
+    for(int i = 0; i < temperatures; i++){
+        cout << "Skriv inn temperatur nr " << (i+1) << ":";
+        cin >> temp; //tar inn temperatur
 
+        //sammenlikner med de forskjellige avgransningene og incrementer variabelen med 1 ved match
+        if(temp < 10){
+            below++;
+        }
+        else if(temp > 20){
+            above++;
+        }
+        else{
+            between++;
+        }
+    }
 
-    if(temp < 10){
-        below++;
-    }
-    else if(temp > 20){
-        above++;
-    }
-    else{
-        between++;
-    }
+    cout << "Antall under 10 er " << below << endl;
+    cout << "Antall mellom 10 og 20 er " << between << endl;
+    cout << "Antall over 20 er " << above << endl;
 
     return 0;
 }
